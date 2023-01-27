8dba7f61555b886f66b8724a6a933ed6a199db25
David Orchard <orchard@pacificspirit.com>
2023-01-27 13:42:11 -0800

    getElementsByTagName

--- greatfrontend/getElementsByTag/getElements.js
+++ greatfrontend/getElementsByTag/getElements.js
@@ -1,30 +1,37 @@
-import './style.css'
-
 /**
  * @param {Element} rootElement
  * @param {string} tagName
  * @return {Array<Element>}
  */
- const document = new DOMParser().parseFromString(
-  `<div id="foo">
-    <span>Span</span>
-    <p>Paragraph</p>
-    <div id="bar">Div</div>
-  </div>`,
-  'text/html',
-);
-
-export default function getElementsByTagName(rootElement, tagName) {
-  if(!rootElement.childElementCount) return rootElement;
-  const children = rootElement.childNodes;
-  console.log('children', children);
-  for (const node of children) {
-    console.log('node', node);
+/**
+ * @param {Element} rootElement
+ * @param {string} tagName
+ * @return {Array<Element>}
+ */
+const document = new DOMParser().parseFromString(
+    `<div id="foo">
+      <span>Span</span>
+      <p>Paragraph</p>
+      <div id="bar">Div</div>
+    </div>`,
+    'text/html',
+  );
+  
+  const traverse = (elem, tagName) => {
+    if(!elem) return;
+  
+    return [...(elem.tagName === tagName ? [elem] : []), ...Array.from(elem.children).reduce((acc, child) => {
+      const trav = traverse(child, tagName);
+      return (trav?.length) ? acc.concat(trav) : acc;
+    }, [])];
   }
-
-  if (!children?.length) return children;
-  return [...children].map((child) => getElementsByTagName(children, tagName));
-}
-
-console.log(getElementsByTagName(document.body, 'div'));
-// [div#foo, div#bar] <-- This is an array of elements.
+  export default function getElementsByTagName(rootElement, tagNameParam) {
+    const tagName = tagNameParam.toUpperCase();
+    return !rootElement?.children?.length ? [] : Array.from(rootElement.children).reduce((acc, child) => {
+      const trav = traverse(child, tagName);
+      return (trav?.length) ? acc.concat(trav) : acc;
+    }, []);
+  }
+  
+  console.log(getElementsByTagName(document.body, 'div'));
+  // [div#foo, div#bar] <-- This is an array of elements.
