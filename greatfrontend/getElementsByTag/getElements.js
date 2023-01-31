1366c45ffe938e12e44a62c5b4c4502be5dab614
David Orchard <orchard@pacificspirit.com>
2023-01-31 09:38:26 -0800

    update getElementsByTag

--- greatfrontend/getElementsByTag/getElements.js
+++ greatfrontend/getElementsByTag/getElements.js
@@ -33,5 +33,32 @@ const document = new DOMParser().parseFromString(
     }, []);
   }
   
+    // export default function getElementsByTagName(rootElement, tagNameParam) {
+    //   const elements = [];
+    //   const tagName = tagNameParam.toUpperCase();
+    
+    //   function traverse(element) {
+    //     if (element == null) {
+    //       return;
+    //     }
+    
+    //     if (element.tagName === tagName) {
+    //       elements.push(element);
+    //     }
+    
+    //       console.log('elem children', element.children)
+    
+    //     for (let i = 0; i < (element.children || []).length; i++) {
+    //       traverse(element.children[i]);
+    //     }
+    //   }
+    
+    //   for (let i = 0; i < (rootElement.children || []).length; i++) {
+    //     traverse(rootElement.children[i]);
+    //   }
+    
+    //   return elements;
+    // }
+    
   console.log(getElementsByTagName(document.body, 'div'));
   // [div#foo, div#bar] <-- This is an array of elements.
